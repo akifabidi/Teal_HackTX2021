6c9d263ec61fd936a0807827bb038b15ef6a156a
omar imtiaz <oaimtiaz@utexas.edu>
2021-10-30 16:39:00 -0500

    Finished first version of matcher

--- Users/akifa/OneDrive/Documents/Hackathons/HackTx_2021/Teal/routes/matchRouter.js
+++ Users/akifa/OneDrive/Documents/Hackathons/HackTx_2021/Teal/routes/matchRouter.js
@@ -8,7 +8,15 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/buy', (req, res) => {
-  
+  result =matcher.addBuyOrder(req.body.walletId, req.body.creatorId, req.body.quantity, req.body.price);
+  //Add message giving status update !! 
+  res.json(out);
 });
+
+router.post('/sell', (req, res) => {
+  out = matcher.addSellOrder(req.body.walletId, req.body.creatorId, req.body.quantity, req.body.price);
+  res.send("ok");
+});
+
 
 module.exports = router;
